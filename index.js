19bf229ba8342ca1bdecbf3c7f857ab0819e29e6
Dan McVay <danielmcvaymedia@gmail.com>
2025-08-30 01:02:51 +0100

    Implemented Human Choice
    user will be met with prompt to choose their move

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function getComputerChoice() {
     console.log("Scissors");
   }
 }
+
+function getHumanChoice() {
+  let choice = prompt("Choose your move! /1 = Rock /2 = Paper /3 = Scissors");
+  if (choice === "1") {
+    console.log("Rock");
+  } else if (choice === "2") {
+    console.log("Paper");
+  } else if (choice === "3") {
+    console.log("Scissors");
+  }
+}
+
+let computer = 0;
+let humanScore = 0;
